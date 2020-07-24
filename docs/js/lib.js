76e7294fc4a049ebdb7041333618b0623b11054b
Restyled.io <commits@restyled.io>
2020-07-24 08:53:39 +0000

    Restyled by clang-format

--- docs/js/lib.js
+++ docs/js/lib.js
@@ -1,6 +1,5 @@
-function labelDetail(detailurl, label_id,breadcrumb_label){
+function labelDetail(detailurl, label_id, breadcrumb_label) {
 
-        window.location.href=detailurl+"?label_id="+label_id+"&breadcrumb_label="+breadcrumb_label;
-
+  window.location.href = detailurl + "?label_id=" + label_id +
+                         "&breadcrumb_label=" + breadcrumb_label;
 }
-
